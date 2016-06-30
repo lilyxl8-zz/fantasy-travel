6c50b11ebc205d87f539e0cfa67721de5f4accc7
Lily Zhang <ellabailasola@gmail.com>
2016-06-30 15:14:11 -0400

    clean gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/**/*.scss')
+  return gulp.src('app/css/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({
@@ -21,7 +21,7 @@ gulp.task('browserSync', function() {
 
 // run browserSync and compile sass before we start watching a folder
 gulp.task('watch', ['browserSync', 'sass'], function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/css/**/*.scss', ['sass']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
 })
