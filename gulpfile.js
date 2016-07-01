9f7c45638c54fd6acee94fc04158be43207b55b3
Lily Zhang <ellabailasola@gmail.com>
2016-06-30 20:48:37 -0400

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var gulpIf = require('gulp-if');
 var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
@@ -6,20 +7,12 @@ var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(){
   return gulp.src('app/css/**/*.scss')
-    .pipe(sass())
+    .pipe(sass()) // compiles to CSS
+    .pipe(cssnano()) // minifies CSS
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
-});
-
-gulp.task('useref', function(){
-  return gulp.src('app/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    // Minifies only if it's a CSS file
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('browserSync', function() {
